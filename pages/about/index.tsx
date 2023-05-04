9de7b3ed1e6d394412574360cb591810ed8ffd2f
shunki-chrome <fateveriki@gmail.com>
2023-05-04 20:47:03 +0900

    文言追加

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,21 @@
+import { Box, Typography } from "@mui/material";
+import styles from "../../styles/Home.module.css";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Box
+      sx={{
+        width: "80%",
+        maxWidth: "500px",
+        margin: "0 auto",
+      }}
+    >
+      {/* <FirstLogo /> */}
+      <Box className={styles.home_contents}>
+        <Typography variant="h2"></Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default About;
